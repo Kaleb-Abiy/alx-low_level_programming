c8dc93722b7fa4bc9d13e788d3baaa20edba80d2
root <kalebabiy22>
2021-08-27 12:55:43 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: 0 for success
+  */
+int main(void)
+{
+
+	int n = 'a';
+
+	for ( ; n <= 'z' ; n++)
+	{	
+		if (n != 'q' && n != 'e')
+			putchar(n);
+								}
+	}		
+	putchar(10);
+	return (0);
+
+}
