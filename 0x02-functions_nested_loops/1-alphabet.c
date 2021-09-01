1561bf07b6c083afa57b60cdb0a4fab8e8b89552
root <kalebabiy22>
2021-09-01 00:02:26 -0700

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Print alphabet
+  *
+  * Return: (void)
+  *
+  */
+void print_alphabet(void)
+
+{
+	char sh = 'a';
+
+	while (sh <= 'z')
+	{
+		_putchar(sh);
+		sh++;
+	}
+	_putchar('\n');
+}
